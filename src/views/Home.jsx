cc2d512f629da0e3bbef1e08839aa8c077d0b1ad
Kaytlin Valorosi <kvalorosi@yahoo.com>
2023-07-11 14:48:07 -0700

    first

---
+++ src/views/Home.jsx
@@ -0,0 +1,8 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>Woohoo you made it home!!</h1>
+        </div>
+    )
+}
+export default Home;
---
+++ src/views/Other.jsx
@@ -0,0 +1,8 @@
+const Other = () => {
+    return (
+        <div>
+            <h1>Hmm this is a mystery page for now!!!</h1>
+        </div>
+    )
+}
+export default Other;
